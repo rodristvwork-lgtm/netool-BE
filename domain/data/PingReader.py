2195d7628828a07357cafac97005ef0a1c5e4e62
Rodriguez JHORDAN <rodriguez.jhordan@alten.it>
2025-12-18 12:13:22 +0100

    mac4p4: added ping reader data component

---
+++ domain/data/PingReader.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data/ping"
+
+def fetch_ping_data():
+    contents = []
+    try:
+        if not BASE_DIR.exists() or not DATA_DIR.is_dir():
+            raise FileNotFoundError(f"Data directory not found: {DATA_DIR}")
+        
+        files = list(DATA_DIR.glob("ping_*.txt"))
+        
+        if not files:
+            raise FileExistsError(f"No Ping Data files found in: {DATA_DIR}")
+        
+        for file in files:
+            try:
+                text = file.read_text(encoding="utf-8")
+                contents.append(text)        
+            except Exception as e:
+                print(f"Error reading {file.name} : {e} ")
+                
+    except Exception as e:
+        print(f"Data fetch failed: {e}") 
+    
+    
+    return contents[0][0]
+            
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 from domain.model.model import Ping
+from domain.data.PingReader import fetch_ping_data
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    ping_responde = Ping(seconds=300, byte=1500, server="ebay.com")
+    
+    single_value = fetch_ping_data()
+    ping_responde = Ping(seconds=300, byte=1500, server=single_value)
     return jsonify(ping_responde.__dict__)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
