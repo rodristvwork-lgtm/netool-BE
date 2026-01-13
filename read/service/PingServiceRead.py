10574806970d4c0c6f6e87d49b3953d261e9c3e5
Rodriguez JHORDAN <rodriguez.jhordan@alten.it>
2026-01-13 10:52:01 +0100

    mac8p8: added list Ping entity

--- read/service/PingServiceRead.py
+++ read/service/PingServiceRead.py
@@ -2,7 +2,6 @@ from read.read.PingDataRead import read_ping_data
 from domain.model.Ping import Ping
 import re
 
-# Takes the line with PING inside -> It recognize the starting of Ping Results
 ping_starter_regex = r'^PING\s+.+\s+bytes of data\.$'
 ping_byte_regex = r"^(\d+)\s+bytes"
 ping_time_regex = r'time=(\d+(?:\.\d+)?)\s*ms'
@@ -10,42 +9,32 @@ ping_icmp_regex = r'icmp_seq=(\d+)'
 ping_statistic_regex = r'^---(?:\s+([^\s]+))?\s+ping statistics ---$'
 ping_timestamp_regex = r'^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d+$'
 
-
 def findAllPingResults ():
     
-    # Each run is a file with multiple ping results
     files_ping_data = read_ping_data()
     
-    # Here it reads all the Files which can contain multiple data
-    for file_data in files_ping_data:    
-        
-        # Here takes one File which can contain multiple Ping runs
-        file_data = file_data.strip().splitlines()
-        
-        # File Builder -> it returns a List of Ping
-        listPingDataBuilder(file_data)
-         
+    for single_file_ping_data in files_ping_data:            
+        single_file_ping_data = single_file_ping_data.strip().splitlines()
+        listPingConstructor(single_file_ping_data) 
     return False
 
-
-def listPingDataBuilder (file_data:str):
+def listPingConstructor (single_file_ping_data:str):
     
-    instance = None
+    ping = None
     reading_process = False
+    list_ping = []
     
-    for line in file_data:
+    for line in single_file_ping_data:
         
-        if re.match(pattern = ping_starter_regex, string = line):
-            
-            instance = Ping()
-            reading_process = True
-            continue
-              
         icmp_match = re.search(pattern= ping_icmp_regex , string = line)
-            
+        timestamp_match = re.search(pattern= ping_timestamp_regex , string = line)
+
+        if re.match(pattern = ping_starter_regex, string = line):
+            ping = Ping()
+            reading_process = True
+                                    
         if icmp_match and reading_process:
             
-            # Tuple savings
             icmp = icmp_match.group(1)
             ping_tuple_record = []
             byte_match = re.search(pattern= ping_byte_regex , string = line)
@@ -56,8 +45,7 @@ def listPingDataBuilder (file_data:str):
                 ping_tuple_record.append(byte)
             else:
                 ping_tuple_record.append("0")
-                print("not byte found")
-            
+                         
             icmp = icmp_match.group(1)
             ping_tuple_record.append(icmp)
             
@@ -67,8 +55,19 @@ def listPingDataBuilder (file_data:str):
                 
             else:              
                 ping_tuple_record.append("0")
-                print("not time found")
                        
-            instance.time_history.append(ping_tuple_record)
+            ping.time_history.append(ping_tuple_record)
+        
+        if timestamp_match and reading_process:
             
+            timestamp = timestamp_match.group()
+            ping.timestamp = timestamp
+            reading_process = False
+            list_ping.append(ping)
+              
+    for ping in list_ping:
+        
+        print(ping.time_history)
+        print("XXXXXXXX Whatever")
+       
     return None
